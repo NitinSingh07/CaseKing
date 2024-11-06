1f8fb4ff96168c8e6f11b2bf5f44b9f4cea14d8b
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-11-07 00:09:45 +0530

    Remove unused import of cookies in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "./ui/button";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { cookies } from "next/headers";
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession();
